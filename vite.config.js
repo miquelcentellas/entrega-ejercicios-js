423e0f46caf5218d6626100fdbff1894282f0233
miquel <miquelcentellas@hotmail.com>
2026-01-19 13:03:27 +0100

    corrigiendo vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,22 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/entrega-ejercicios-js/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        ejercicio1: resolve(__dirname, "ejercicio1.html"),
+        ejercicio2: resolve(__dirname, "ejercicio2.html"),
+        ejercicio3: resolve(__dirname, "ejercicio3.html"),
+        ejercicio4: resolve(__dirname, "ejercicio4.html"),
+        ejercicio5: resolve(__dirname, "ejercicio5.html"),
+        ejercicio6: resolve(__dirname, "ejercicio6.html"),
+        ejercicio7: resolve(__dirname, "ejercicio7.html"),
+        ejercicio8: resolve(__dirname, "ejercicio8.html"),
+        ejercicio9: resolve(__dirname, "ejercicio9.html"),
+      },
+    },
+  },
 });
